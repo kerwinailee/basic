ca3b3180a39ced91ca0589dc766992bcba9626f2
黄晓南 <xiaonan.huang@achievo.cn>
2019-05-07 18:04:27 +0800

    应用结构-控制器

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'language' => 'zh-CN',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
